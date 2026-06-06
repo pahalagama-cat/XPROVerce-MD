4bccc422c29b03bd9a0ac9c418b6f5eaad871846
pahalagama-cat <sahanpahalagama2021@gmail.com>
2026-06-06 07:00:18 +0530

    Update session.js

--- session.js
+++ session.js
@@ -37,6 +37,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session",
+SESSION_ID: process.env.SESSION_ID || "XPRO-MD~1780709284371_m0s9oqhtw",
 PORT: process.env.PORT || "8000"
 };
